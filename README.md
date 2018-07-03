e75162c5c37fb84bcc180a748de41b6484b2650b
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2018-07-03 21:21:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# getlocalhostnames # getlocalhostnames
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"sync"
+)
+
+func doSomething(u int) {
+
+	str := fmt.Sprintf("192.168.0.%d", u)
+	//fmt.Println(str)
+	out, err := exec.Command("dig", "+short", "+time=1", "-x", str, "@224.0.0.251", "-p", "5353").Output()
+	if err != nil {
+		//log.Printf("Exec fail: %v", err)
+	} else {
+		fmt.Print(string(out))
+	}
+	// dig -x ip @224.0.0.251 -p 5353
+	//time.Sleep(2 * time.Second)
+}
+
+func main() {
+
+	limit := make(chan struct{}, 20)
+
+	var wg sync.WaitGroup
+	for i := 1; i < 255; i++ {
+		ii := i
+		wg.Add(1)
+		go func() {
+			limit <- struct{}{}
+			defer wg.Done()
+			doSomething(ii)
+			<-limit
+		}()
+		//time.Sleep(200 * time.Millisecond)
+	}
+	wg.Wait()
+}
