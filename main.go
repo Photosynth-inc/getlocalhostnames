209c182a68e0d53a0fcb7ffa2a1f4a42d24e3b21
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2018-07-05 11:27:49 +0900

    Improve performance

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func doSomething(u int) {
 
 	str := fmt.Sprintf("192.168.0.%d", u)
 	//fmt.Println(str)
-	out, err := exec.Command("dig", "+short", "+time=1", "-x", str, "@224.0.0.251", "-p", "5353").Output()
+	out, err := exec.Command("dig", "+short", "+time=1", "+tries=1", "-x", str, "@224.0.0.251", "-p", "5353").Output()
 	if err != nil {
 		//log.Printf("Exec fail: %v", err)
 	} else {
@@ -22,7 +22,7 @@ func doSomething(u int) {
 
 func main() {
 
-	limit := make(chan struct{}, 20)
+	limit := make(chan struct{}, 50)
 
 	var wg sync.WaitGroup
 	for i := 1; i < 255; i++ {
