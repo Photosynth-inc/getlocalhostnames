a2a511fc85a0d6db59ee03176b8c305ccc1c0786
daikw <hazuhi@gmail.com>
2021-07-06 13:43:51 +0900

    :+1: add segment option / add help text

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ const (
 	QUEUE_WAIT = 200
 )
 
+const HELP_TEXT = "reverse lookup `.local` hostnames. Usage:\n    go run main.go [-v] [-h] [-s segment (default: 192.168.101)]"
+
 func dig(ip string, verbose bool) {
 	out, err := exec.Command("dig", "+short", "+time=1", "+tries=1", "-x", ip, "@224.0.0.251", "-p", "5353").Output()
 	if err != nil && verbose {
@@ -27,11 +29,21 @@ func dig(ip string, verbose bool) {
 func main() {
 	limit := make(chan struct{}, 50)
 
+	segment := "192.168.101"
 	verbose := false
 	for _, v := range os.Args {
 		switch v {
-			case "-v" :
+			case "-v":
 				verbose = true
+			case "-h":
+				fmt.Println(HELP_TEXT)
+				return
+			case "-s":
+				segment = "selected"
+			default:
+				if segment == "selected" {
+					segment = v
+				}
 		}
 	}
 
